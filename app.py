106255f55b22dea908ce160cc165801a8b87e33f
MFaizSyed <72081457+MFaizSyed@users.noreply.github.com>
2021-05-11 00:50:01 -0400

    app.py updated

--- app.py
+++ app.py
@@ -9,6 +9,8 @@ app.config['SESSION_TYPE'] = 'filesystem'
 
 Session(app)
 
+socketio = SocketIO(app, manage_session=False)
+
 @app.route('/', methods=['GET','POST'])
 def index():
     return render_template('index.html')
@@ -28,5 +30,25 @@ def chat():
         else:
             return redirect(url_for('index'))
 
+@socketio.on('join', namespace='\chat')
+def join(message):
+    room = session.get('room')
+    join_room(room)
+    emit('status', {'msg': session.get('username') + 'has entered the room.'}, room=room)
+
+@socketio.on('text', namespace='/chat')
+def text(message):
+    room = session.get('room')
+    emit('message', {'msg: ' + session.get('username') + ' : ' + message['msg']}, room=room)
+
+@socketio.on('left', namespace='/chat')
+def left(message):
+    room = session.get('room')
+    username = session.get('username')
+    leave_room(room)
+    session.clear()
+    emit('status', {'msg': username + ' has left the room.'}, room=room)
+
+
 if __name__=='__main__':
-    app.run()
+    socketio.run(app)
